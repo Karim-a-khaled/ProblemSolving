9bf9f8532ed1ea7e259d28d81b636afcd05bf90b
karim-joacademy <karim.joacademy@gmail.com>
2025-04-16 11:48:54 +0300

    problem #3 done

--- ProblemSolving/Levels/Level1.cs
+++ ProblemSolving/Levels/Level1.cs
@@ -30,4 +30,22 @@ public class Level1
             Console.WriteLine();
         }
     }
+    
+    public static void InvertedLetterDEC(int number)
+    {
+        char[] letters = { 
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 
+            'V', 'W', 'X', 'Y', 'Z' 
+        };    
+        
+        for (int i = number; i >= 0; i--)
+        {
+            for (int j = 0; j <= number; j++)
+            {
+                Console.Write(letters[i]);
+            }
+            Console.WriteLine();
+            number--;
+        }
+    }
 }
--- ProblemSolving/Program.cs
+++ ProblemSolving/Program.cs
@@ -7,6 +7,7 @@ class Program
     static void Main(string[] args)
     {
         //Level1.InvertedNumberASC(4);
-        Level1.InvertedLetterASC(4);
+        //Level1.InvertedLetterASC(4);
+        Level1.InvertedLetterDEC(4);
     }
 }
